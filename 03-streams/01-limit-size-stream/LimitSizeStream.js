db2778e940a06d92ed23d807511036849466e1d5
Dmitry Gusev <dmivlagus@gmail.com>
2021-03-28 19:55:56 +0300

    Create transform stream

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -2,12 +2,33 @@ const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
+
+  /**
+   * Максимальный размер передаваемых данных в байтах
+   * @type {number}
+   */
+  #limit = 0;
+  #totalSize = 0;
+
   constructor(options) {
     super(options);
+
+    if (typeof options.limit !== "undefined") {
+      this.#limit = options.limit;
+    } else {
+      throw new Error('Missing options.limit')
+    }
   }
 
   _transform(chunk, encoding, callback) {
+    this.#totalSize += chunk.length;
 
+    if (this.#totalSize > this.#limit) {
+      callback(new LimitExceededError());
+      return;
+    }
+
+    callback(null, chunk);
   }
 }
 
