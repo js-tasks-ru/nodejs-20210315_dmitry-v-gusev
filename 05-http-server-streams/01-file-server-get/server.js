8b72b77b940bea0d5798b8263853d3297d89f751
Gusev Dmitry (ML) <dgusev@megalabs.ru>
2021-03-31 13:57:57 +0300

    Create simple server

--- 05-http-server-streams/01-file-server-get/server.js
+++ 05-http-server-streams/01-file-server-get/server.js
@@ -1,10 +1,11 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const {createReadStream} = require('fs');
 
 const server = new http.Server();
 
-server.on('request', (req, res) => {
+server.on('request', async (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
 
   const filepath = path.join(__dirname, 'files', pathname);
@@ -12,6 +13,25 @@ server.on('request', (req, res) => {
   switch (req.method) {
     case 'GET':
 
+      if (/\/(.)/.test(pathname)) {
+        res.statusCode = 400;
+        return res.end('Bad request');
+      }
+
+      const readStream = createReadStream(filepath);
+
+      try {
+        for await (const chunk of readStream) {
+          res.write(chunk);
+        }
+      } catch (error) {
+        if (error.code === 'ENOENT') {
+          res.statusCode = 404;
+          return res.end('Not found');
+        }
+      }
+
+      res.end();
       break;
 
     default:
